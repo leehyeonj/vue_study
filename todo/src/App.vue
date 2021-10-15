cd1f1efee1fed4fbc831166df946d44d1c282fd9
leehyeonju <billaa@naver.com>
2021-10-15 19:49:13 +0900

    todo app 만들기
    아이템 추가

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,25 +1,48 @@
 <template>
-  <div>
-    Count : {{ count }}, double : {{ double }} <br>
-    <button @click="increment">Increase</button>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input 
+    v-model="todoText"
+    type="text" 
+    class="w-100" 
+    placeholder="Type todo"
+    @keyup.enter="addTodo"
+    >
+    <hr>
+  <Todo 
+  v-for="todo in todos" 
+  :key="todo.id" 
+  :todo="todo"/>
+   <Todo/>
+    <Todo/>
   </div>
 </template>
 
 <script>
+import Todo from '@/components/Todo.vue'
+
 export default {
-  data(){
-    return {
-      count: 0,
-    }
+  components : {
+    Todo
   },
-  computed : {
-    double(){
-      return this.count * 2;
+  data() {
+    return {
+      todoText: '',
+      todos: [
+        {id: 1, text: 'buy a car', checked: false},
+         {id: 2, text: 'play game', checked: false},
+      ]
     }
   },
   methods : {
-    increment(){
-      this.count++;
+    addTodo(e) {
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false
+
+      });
+      this.todoText = '';
     }
   }
 }
