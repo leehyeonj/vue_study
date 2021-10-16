09a3cde31dc3b52736b09113aa731e37ec199e0d
leehyeonju <billaa@naver.com>
2021-10-16 23:03:06 +0900

    form 데이터 바인딩
    패스트 캠퍼스

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,22 +1,33 @@
 <template>
   <div>
-    <form action="">
+    <form v-on:submit="submitForm">
       <div>
         <label for="username">Id:</label>
-        <input id="username" type="text">
+        <input id="username" type="text" v-model="username">
       </div>
       <div>
         <label for="password">pw: </label>
-        <input id="password" type="password">
+        <input id="password" type="password" v-model="password">
       </div>
-      <button>로그인</button>
+      <button type="submit">로그인</button>
     </form>
   </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+      username : "",
+      password : "",
+    }
+  },
+  methods : {
+    submitForm(event) {
+      event.preventDefault()
+      console.log('submitted')
+    }
+  }
 }
 </script>
 
