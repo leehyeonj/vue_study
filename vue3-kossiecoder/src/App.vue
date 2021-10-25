ccbf81c56826a079cd31aab80912bf901571acec
hyeonju <billaa@naver.com>
2021-10-25 10:08:01 +0900

    vue3-kossiecoder: 05함수사용하기

--- vue3-kossiecoder/src/App.vue
+++ vue3-kossiecoder/src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="name">
+    {{greeting(name)}}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+ setup(){
+   const name = 'lee';
+
+   const greeting = (name) =>{
+     return 'hello' + name;
+   }
+   return{
+     name,
+     greeting
+   }
+ }
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+
 </style>
