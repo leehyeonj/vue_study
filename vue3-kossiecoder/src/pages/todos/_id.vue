2c57b2b633dc4215083c224954a688a8d12513ab
hyeonju <billaa@naver.com>
2021-10-28 12:10:25 +0900

    Vue3-kossiecoder : 48~50 todo 디테일 페이지 컴포넌트 만들기
    디테일에서 토글하고 저장하기

--- vue3-kossiecoder/src/pages/todos/_id.vue
+++ vue3-kossiecoder/src/pages/todos/_id.vue
@@ -1,13 +1,88 @@
 <template>
-  <div>Todo Page</div>
+  <h1>To-Do Page</h1>
+  <div v-if="loading">Loading..</div>
+  <form v-else @submit.prevent="onSave">
+    <div class="row">
+      <div class="col-6">
+        <div class="form-group">
+          <label>Subject</label>
+          <input v-model="todo.subject" type="text" class="form-control" />
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="form-group">
+          <label>Status</label>
+          <div>
+            <button
+              class="btn"
+              type="button"
+              :class="todo.completed ? 'btn-success' : 'btn-danger'"
+              @click="toggleTodoStatus"
+            >
+              {{ todo.completed ? "Completed" : "Incomplete" }}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <button type="submit" class="btn btn-primary mt-2">Save</button>
+    <button class="btn btn-outline-dark ml-2 mt-2" @click="moveToTodoListPage">
+      Cancel
+    </button>
+  </form>
 </template>
 
+
 <script>
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
+import axios from "axios";
+import { ref } from "@vue/reactivity";
 export default {
   setup() {
-    const route = useRoute();
-    console.log(route.params.id);
+    const route = useRoute(); //params id 가져오기
+    const router = useRouter();
+    const todo = ref(null);
+    const loading = ref(true);
+    const todoId = route.params.id;
+
+    //한개의 todo 데이터 가져오기
+    const getTodo = async () => {
+      const res = await axios.get("http://localhost:3000/todos/" + todoId);
+      todo.value = res.data;
+      loading.value = false;
+    };
+
+    //상세 페이지에서 토글하기
+    const toggleTodoStatus = () => {
+      todo.value.completed = !todo.value.completed;
+    };
+
+    //리스트 페이지로 이동하기
+    const moveToTodoListPage = () => {
+      router.push({
+        name: "Todos",
+      });
+    };
+
+    //저장하기
+    const onSave = async () => {
+      const res = await axios.put(`http://localhost:3000/todos/${todoId}`, {
+        subject: todo.value.subject,
+        completed: todo.value.completed,
+      });
+      console.log(res);
+    };
+    getTodo();
+
+    //return
+    return {
+      todo,
+      loading,
+      toggleTodoStatus,
+      moveToTodoListPage,
+      onSave,
+    };
   },
 };
 </script>
