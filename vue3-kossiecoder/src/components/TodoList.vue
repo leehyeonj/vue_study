e8e937b4198e482b384b30849a8885ff2f330563
hyeonju <billaa@naver.com>
2021-10-28 10:39:12 +0900

    vue3-kossiecoder : 30 db 에서 데이터 삭제하기

--- vue3-kossiecoder/src/components/TodoList.vue
+++ vue3-kossiecoder/src/components/TodoList.vue
@@ -1,58 +1,50 @@
 <template>
-     <div 
-        class="card mt-2" 
-        v-for="(todo,index) in todos" 
-        :key="todo.id">
-
-        <div class="card-body p-2 d-flex align-items-center">
-        <div class="form-check flex-grow-1">
-            <input type="checkbox"
-            class="form-check-input"
-            :value="todo.completed"
-            @change="toggleTodo(index)"
-            >
-            <label class="form-check-label"
-            :class="{todo: todo.completed}"
-            >
-            {{ todo.subject}}
-            </label>
-        </div>
-        <div>
-            <button 
-            class="btn btn-danger btn-sm"
-            @click="deleteTodo(index)"
-            >delete</button>
-        </div>
-        </div>
+  <div v-for="(todo, index) in todos" :key="todo.id" class="card mt-2">
+    <div class="card-body p-2 d-flex align-items-center">
+      <div class="form-check flex-grow-1">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          :value="todo.completed"
+          @change="toggleTodo(index)"
+        />
+        <label class="form-check-label" :class="{ todo: todo.completed }">
+          {{ todo.subject }}
+        </label>
+      </div>
+      <div>
+        <button class="btn btn-danger btn-sm" @click="deleteTodo(index)">
+          Delete
+        </button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-   props : {
-       todos: {
-           type: Array,
-           required : true
-       }
-   },
-   emits :['toggle-todo', 'delete-todo'],
+  props: {
+    todos: {
+      type: Array,
+      required: true,
+    },
+  },
+  emits: ["toggle-todo", "delete-todo"],
+  setup(props, { emit }) {
+    const toggleTodo = (index) => {
+      emit("toggle-todo", index);
+    };
 
-   setup(props, {emit}){
-       const toggleTodo = (index) =>{
-           emit('toggle-todo', index)
-       }
-       const deleteTodo = (index) =>{
-           emit('delete-todo', index)
-       }
-
-       return{
-           toggleTodo,
-           deleteTodo
-       }
-   }
+    const deleteTodo = (index) => {
+      emit("delete-todo", index);
+    };
+    return {
+      toggleTodo,
+      deleteTodo,
+    };
+  },
 };
 </script>
 
-<style lang="scss" scoped>
-
+<style>
 </style>
--- vue3-kossiecoder/src/components/TodoSimpleForm.vue
+++ vue3-kossiecoder/src/components/TodoSimpleForm.vue
@@ -1,58 +1,49 @@
 <template>
-  <form @submit.prevent="onSubmit" >
-  <div  class="d-flex">
-    <div class="flex-grow-1 mr-2">
-        <input 
-        class="form-control"
-        type="text" 
-        v-model="todo"
-        placeholder="type new todo"
-        >
-    </div>
-      <div>
-        <button 
-        class="btn btn-primary"
-        type="submit"
-        >add
-        </button>
+  <form @submit.prevent="onSubmit">
+    <div class="d-flex">
+      <div class="flex-grow-1 mr-2">
+        <input
+          class="form-control"
+          type="text"
+          v-model="todo"
+          placeholder="Type new to-do"
+        />
       </div>
-  </div>
-    <div v-show="hasError" style="color:red">아무것도 적지 않았군</div>
+      <div>
+        <button class="btn btn-primary" type="submit">Add</button>
+      </div>
+    </div>
+    <div v-show="hasError" style="color: red">This field cannot be empty</div>
   </form>
 </template>
 
 <script>
-import {ref} from 'vue';
-
+import { ref } from "vue";
 export default {
-    emits : ['add-todo'],
-    setup(props, context){
-        const todo = ref('');
-        const hasError = ref(false);
-        const onSubmit = () =>{
-            if(todo.value === ''){
-                hasError.value = true
-            }
-            else{
-                context.emit('add-todo', {
-                    id: Date.now(),
-                    subject : todo.value,
-                    completed : false,
-                });
-                hasError.value = false;
-                todo.value = '';
-            }
-        }
-
+  emits: ["add-todo"],
+  setup(props, { emit }) {
+    const todo = ref("");
+    const hasError = ref(false);
+    const onSubmit = () => {
+      if (todo.value === "") {
+        hasError.value = true;
+      } else {
+        emit("add-todo", {
+          id: Date.now(),
+          subject: todo.value,
+          completed: false,
+        });
+        hasError.value = false;
+        todo.value = "";
+      }
+    };
     return {
-        todo, 
-        hasError,
-        onSubmit
-    }
-    }
-}
+      todo,
+      hasError,
+      onSubmit,
+    };
+  },
+};
 </script>
 
-<style lang="">
-    
-</style>
+<style></style>
