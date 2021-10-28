d5efce488bd4aa180172660136ea07eb642a7e0e
hyeonju <billaa@naver.com>
2021-10-28 11:44:23 +0900

    Vue3-kossiecoder : 45~ 46 useRouter  router.push

--- vue3-kossiecoder/src/App.vue
+++ vue3-kossiecoder/src/App.vue
@@ -1,161 +1,21 @@
 <template>
-  <div class="container">
-    <h2>To-Do List</h2>
-    <input
-      class="form-control"
-      type="text"
-      v-model="searchText"
-      placeholder="Search"
-    />
-    <hr />
-    <TodoSimpleForm @add-todo="addTodo" />
-    <div style="color: red">{{ error }}</div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link class="navbar-brand" :to="{ name: 'Home' }">
+      Kossie Coder
+    </router-link>
 
-    <div v-if="!filteredTodos.length">There is nothing to display</div>
-    <TodoList
-      :todos="filteredTodos"
-      @toggle-todo="toggleTodo"
-      @delete-todo="deleteTodo"
-    />
-    <hr />
-    <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        <li v-if="currentPage !== 1" class="page-item">
-          <a class="page-link" @click="getTodos(currentPage - 1)">Previous</a>
-        </li>
-        <li
-          class="page-item"
-          v-for="page in numberOfPages"
-          :key="page"
-          :class="currentPage === page ? 'active' : ''"
-        >
-          <a class="page-link" @click="getTodos(page)">{{ page }}</a>
-        </li>
-        <li v-if="numberOfPages !== currentPage" class="page-item">
-          <a class="page-link" @click="getTodos(currentPage + 1)">Next</a>
-        </li>
-      </ul>
-    </nav>
-  </div>
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <router-link class="nav-link" to="/todos"> Todos </router-link>
+      </li>
+    </ul>
+  </nav>
+  <router-view />
 </template>
 
 <script>
-import { ref, computed, watchEffect } from "vue";
-import TodoSimpleForm from "./components/TodoSimpleForm.vue";
-import TodoList from "./components/TodoList.vue";
-import axios from "axios";
-export default {
-  components: {
-    TodoSimpleForm,
-    TodoList,
-  },
-  setup() {
-    const todos = ref([]);
-    const error = ref("");
-    const numberOfTodos = ref(0);
-    const limit = 10;
-    const currentPage = ref(1);
-
-    //값이 변경이 되면 이 함수가 다시 실행이 된다.
-    //reactive 한 state 에만 적용이 된다.
-    watchEffect(() => {
-      console.log(currentPage.value);
-    });
-
-    const numberOfPages = computed(() => {
-      return Math.ceil(numberOfTodos.value / limit);
-    });
-    //get
-    const getTodos = async (page = currentPage.value) => {
-      currentPage.value = page;
-      try {
-        const res = await axios.get(
-          `http://localhost:3000/todos?_page=${page}&_limit=${limit}`
-        );
-        numberOfTodos.value = res.headers["x-total-count"];
-        console.log(res.headers["x-total-count"]);
-        todos.value = res.data;
-      } catch (err) {
-        console.log(err);
-        error.value = "Something went wrong.";
-      }
-    };
-    getTodos();
-
-    //add
-    const addTodo = async (todo) => {
-      // 데이터베이스 투두를 저장
-      error.value = "";
-      try {
-        const res = await axios.post("http://localhost:3000/todos", {
-          subject: todo.subject,
-          completed: todo.completed,
-        });
-        todos.value.push(res.data);
-      } catch (err) {
-        console.log(err);
-        error.value = "Something went wrong.";
-      }
-    };
-
-    //delete
-    const deleteTodo = async (index) => {
-      error.value = "";
-      const id = todos.value[index].id;
-      try {
-        await axios.delete("http://localhost:3000/todos/" + id);
-
-        todos.value.splice(index, 1);
-      } catch (err) {
-        console.log(err);
-        error.value = "Something went wrong.";
-      }
-    };
-
-    //toggle
-    const toggleTodo = async (index) => {
-      error.value = "";
-      const id = todos.value[index].id;
-      try {
-        await axios.patch("http://localhost:3000/todos/" + id, {
-          completed: !todos.value[index].completed,
-        });
-      } catch (err) {
-        console.log(err);
-        error.value = "something went wrond";
-      }
-      todos.value[index].completed = !todos.value[index].completed;
-    };
-
-    //search
-    const searchText = ref("");
-    const filteredTodos = computed(() => {
-      if (searchText.value) {
-        return todos.value.filter((todo) => {
-          return todo.subject.includes(searchText.value);
-        });
-      }
-      return todos.value;
-    });
-    return {
-      todos,
-      addTodo,
-      deleteTodo,
-      toggleTodo,
-      searchText,
-      filteredTodos,
-      error,
-      numberOfPages,
-      currentPage,
-      getTodos,
-    };
-  },
-};
+export default {};
 </script>
 
 <style>
-.todo {
-  color: gray;
-  text-decoration: line-through;
-}
 </style>
--- vue3-kossiecoder/src/components/TodoList.vue
+++ vue3-kossiecoder/src/components/TodoList.vue
@@ -1,6 +1,9 @@
 <template>
   <div v-for="(todo, index) in todos" :key="todo.id" class="card mt-2">
-    <div class="card-body p-2 d-flex align-items-center">
+    <div
+      @click="moveToPage(todo.id)"
+      class="card-body p-2 d-flex align-items-center"
+    >
       <div class="form-check flex-grow-1">
         <input
           class="form-check-input"
@@ -22,6 +25,8 @@
 </template>
 
 <script>
+import { useRouter } from "vue-router";
+
 export default {
   props: {
     todos: {
@@ -31,16 +36,34 @@ export default {
   },
   emits: ["toggle-todo", "delete-todo"],
   setup(props, { emit }) {
+    const router = useRouter();
+
+    //complete toggle
     const toggleTodo = (index) => {
       emit("toggle-todo", index);
     };
 
+    //delete
     const deleteTodo = (index) => {
       emit("delete-todo", index);
+    };
+
+    //page move
+    const moveToPage = (todoId) => {
+      console.log(todoId);
+      // router.push("/todos/" + todoId);
+      //장점 : 앱이 커지면 커질 수록 이 로직을 여러 군데에서 사용한다고 하면 path정보가 바뀌면 일일이 바꾸어주지 않아도 된다 .
+      router.push({
+        name: "Todo",
+        params: {
+          id: todoId,
+        },
+      });
     };
     return {
       toggleTodo,
       deleteTodo,
+      moveToPage,
     };
   },
 };
