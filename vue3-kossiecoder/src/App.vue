48b09e203cd87bfb08bb66f0b2e639cb5646eee2
hyeonju <billaa@naver.com>
2021-10-28 11:06:20 +0900

    Vue3-kossiecoder : 32-34 pagination

--- vue3-kossiecoder/src/App.vue
+++ vue3-kossiecoder/src/App.vue
@@ -17,6 +17,25 @@
       @toggle-todo="toggleTodo"
       @delete-todo="deleteTodo"
     />
+    <hr />
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li v-if="currentPage !== 1" class="page-item">
+          <a class="page-link" @click="getTodos(currentPage - 1)">Previous</a>
+        </li>
+        <li
+          class="page-item"
+          v-for="page in numberOfPages"
+          :key="page"
+          :class="currentPage === page ? 'active' : ''"
+        >
+          <a class="page-link" @click="getTodos(page)">{{ page }}</a>
+        </li>
+        <li v-if="numberOfPages !== currentPage" class="page-item">
+          <a class="page-link" @click="getTodos(currentPage + 1)">Next</a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
@@ -33,9 +52,22 @@ export default {
   setup() {
     const todos = ref([]);
     const error = ref("");
-    const getTodos = async () => {
+    const numberOfTodos = ref(0);
+    const limit = 5;
+    const currentPage = ref(1);
+
+    const numberOfPages = computed(() => {
+      return Math.ceil(numberOfTodos.value / limit);
+    });
+    //get
+    const getTodos = async (page = currentPage.value) => {
+      currentPage.value = page;
       try {
-        const res = await axios.get("http://localhost:3000/todos");
+        const res = await axios.get(
+          `http://localhost:3000/todos?_page=${page}&_limit=${limit}`
+        );
+        numberOfTodos.value = res.headers["x-total-count"];
+        console.log(res.headers["x-total-count"]);
         todos.value = res.data;
       } catch (err) {
         console.log(err);
@@ -43,6 +75,8 @@ export default {
       }
     };
     getTodos();
+
+    //add
     const addTodo = async (todo) => {
       // 데이터베이스 투두를 저장
       error.value = "";
@@ -57,6 +91,8 @@ export default {
         error.value = "Something went wrong.";
       }
     };
+
+    //delete
     const deleteTodo = async (index) => {
       error.value = "";
       const id = todos.value[index].id;
@@ -69,6 +105,8 @@ export default {
         error.value = "Something went wrong.";
       }
     };
+
+    //toggle
     const toggleTodo = async (index) => {
       error.value = "";
       const id = todos.value[index].id;
@@ -82,6 +120,8 @@ export default {
       }
       todos.value[index].completed = !todos.value[index].completed;
     };
+
+    //search
     const searchText = ref("");
     const filteredTodos = computed(() => {
       if (searchText.value) {
@@ -99,6 +139,9 @@ export default {
       searchText,
       filteredTodos,
       error,
+      numberOfPages,
+      currentPage,
+      getTodos,
     };
   },
 };
