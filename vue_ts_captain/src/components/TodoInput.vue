a9f07011f5232fa268684f5d12853afad6357e72
hyeonju <billaa@naver.com>
2021-10-28 10:32:26 +0900

    vue_ts_captain: Todoinput

---
+++ vue_ts_captain/src/components/TodoInput.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <label></label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VueStudyTodoinput',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
