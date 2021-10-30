46cc35305a2ae146628c7322f13b49914fdf7ea7
hyeonju <billaa@naver.com>
2021-10-30 15:44:02 +0900

    vue_ts_captain: 할일 추가 버튼 기능 구현

--- vue_ts_captain/src/components/TodoInput.vue
+++ vue_ts_captain/src/components/TodoInput.vue
@@ -1,29 +1,24 @@
 <template>
-    <div>
-        <label></label>
-    </div>
+  <div>
+    <label for="todo-input">오늘 할 일</label>
+    <input id="todo-input" type="text" :value="item" @input="handleInput" />
+    <button @click="addTodo" type="button">add</button>
+  </div>
 </template>
 
 <script>
-export default {
-    name: 'VueStudyTodoinput',
-
-    data() {
-        return {
-            
-        };
+import Vue from "vue";
+export default Vue.extend({
+  props: ["item"],
+  components: {},
+  methods: {
+    handleInput(event) {
+      //   event.target.value;
+      this.$emit("input", event.target.value);
     },
-
-    mounted() {
-        
+    addTodo() {
+      this.$emit("add");
     },
-
-    methods: {
-        
-    },
-};
+  },
+});
 </script>
-
-<style lang="scss" scoped>
-
-</style>
